2a2bc20f7b654ad548ff028be7dc9273041a9081
Sebastian Saner <seb.sa@eblcom.ch>
2025-02-02 20:47:39 +0100

    email better

--- components/Email.tsx
+++ components/Email.tsx
@@ -4,15 +4,8 @@ import { PropsWithChildren, useEffect, useMemo } from 'react';
 interface EmailProps {}
 
 const Email = ({}: PropsWithChildren<EmailProps>) => {
-
-  // const [ mail, setMail ] = useEffect
-
-  const mail = useMemo(() =>
-    [115, 101, 98, 46, 115, 97, 64, 101, 98, 108, 99, 111, 109, 46, 99, 104]
-      .map(x => String.fromCharCode(x)).join(''), []);
-
   return (
-    <p>{mail}</p>
+    <p>seb.sa[ät]eblcom.ch</p>
   );
 }
 
